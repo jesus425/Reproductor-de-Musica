518e06daa11f6331c6aed4fd41118e0ee14e8e00
Jesus Espinoza <jesuseej071107@gmail.com>
2024-10-23 12:03:17 -0400

    guardado

--- index.js
+++ index.js
@@ -8,5 +8,65 @@ let cancion = {
 
     sonando: () => {
         console.log(cancion.letra)
-    } 
+    },
+    cambiartitulo: (nuevotitulo) => {
+        cancion.titulo = nuevotitulo
+    }
 }
+let contenedor = document.getElementById('track-list')
+
+axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
+    console.log(res.data.songs)
+    res.data.songs.map(() => {
+        let div = document.createElement('div')
+        div.setAttribute('class', '')
+        div.innerHTML = `
+        <img src="/Ellipse 7.svg" alt="">
+        <p>Lost on You</p>
+        <p>LP</p>
+        <a href=""></a>
+    </div>
+
+    <div>
+        <img src="/Ellipse 8.svg" alt="">
+        <p>Papercut</p>
+        <p>Linkin Park</p>
+        <a href=""></a>
+    </div>
+
+    <div>
+        <img src="/Ellipse 10.svg" alt="">
+        <p>Dark Beach</p>
+        <p>Pastel ghost</p>
+        <a href=""></a>
+    </div>
+
+    <div>
+        <img src="/Ellipse 11.svg" alt="">
+        <p>Enamorado Tuyo</p>
+        <p>Cuarteto de Nos</p>
+        <a href=""></a>
+    </div>
+
+    <div>
+        <img src="/Ellipse 12.svg" alt="">
+        <p>Snowman</p>
+        <p>Sia</p>
+        <a href=""></a>
+    </div>
+
+    <div>
+        <img src="/Ellipse 9.svg" alt="">
+        <p>Stay with me</p>
+        <p>Miki Matsubara</p>
+        <a href=""></a>
+    </div>
+
+    <div>
+        <img src="/Ellipse 6.svg" alt="">
+        <p>Kingston Town</p>
+        <p>UB40</p>
+        contenedor.appendChild(div
+        `
+    })
+})
