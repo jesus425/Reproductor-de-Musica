1ff97216d0da0faa55b79f460f9f73ce40526f67
Jesus Espinoza <jesuseej071107@gmail.com>
2024-10-23 10:52:20 -0400

    guardado

---
+++ index.js
@@ -0,0 +1,12 @@
+let cancion = {
+    titulo: 'Lost on you',
+    artista: 'LP',
+    album: 'Lost on you',
+    duracion: '3:48',
+    letra: 'hdhdhhdhhdh',
+    portada: 'enlace de imagen',
+
+    sonando: () => {
+        console.log(cancion.letra)
+    } 
+}
