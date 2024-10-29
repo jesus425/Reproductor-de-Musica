34f312e88dfa7945ce45e896ef9ff4b839e3e378
Jesus Espinoza <jesuseej071107@gmail.com>
2024-10-28 23:44:55 -0400

    guardado

--- index.js
+++ index.js
@@ -70,3 +70,26 @@ axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
         `
     })
 })
+
+let progress = document.getElementById("progress")
+        let song = document.getElementById("song")
+        let ctrlicon = document.getElementById("ctrlicon")
+
+        song.onloadedmetadata = function(){
+            progress.max = song.duration;
+            progress.value = song.currentTime;
+        }
+
+        function playPause(){
+            if(ctrlicon.classList.contains("fa-pause")){
+                song.pause();
+                ctrlicon.classList.remove("fa-pause");
+                ctrlicon.classList.add("fa-play");
+         }
+            else{
+                song.play();
+                ctrlicon.classList.add("fa-pause");
+                ctrlicon.classList.remove("fa-play");
+        }
+        
+        }
