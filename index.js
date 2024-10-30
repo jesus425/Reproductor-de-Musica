92011c95e586d6bcf925a78a45c090a231e6fe0d
Jesus Espinoza <jesuseej071107@gmail.com>
2024-10-30 10:20:56 -0400

    guardado

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
+let progress = document.getElementById("progress")
+let song = document.getElementById("song")
+let ctrlicon = document.getElementById("ctrlicon")
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause() {
+    if (ctrlicon.classList.contains("fa-pause")) {
+        song.pause();
+        ctrlicon.classList.remove("fa-pause");
+        ctrlicon.classList.add("fa-play");
+    }
+    else {
+        song.play();
+        ctrlicon.classList.add("fa-pause");
+        ctrlicon.classList.remove("fa-play");
+    }
+
+}
+if (song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+}
+
+progress.onchange = function () {
+    song.play();
+    song.currentTime = progress.value;
+    ctrlicon.classList.add("fa-pause");
+    ctrlicon.classList.remove("fa-play");
+}
+let contenedor = document.getElementById('track-list')
+
+axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
+    console.log(res.data.songs)
+
+    res.data.songs.map((song) => {
+        let div = document.createElement('div')
+        div.setAttribute('class', 'panel-central_cancionescontenido_cancion')
+
+        div.innerHTML = `
+        <img src="https://api.institutoalfa.org/api/songs/image/${song.image.filename}" alt="">
+        <div class="panel-central_cancionescontenidocancion_descripcion">
+            <div class="panel-central_cancionescontenidocancion_descripcion__titulos">
+                <a href="#">${song.title}</a>
+                <a href="#">${song.author}</a>
+            </div>
+        </div>
+        `
+
+        contenedor.appendChild(div)
+    })
+
+})
 let cancion = {
     titulo: 'Lost on you',
     artista: 'LP',
@@ -13,83 +70,5 @@ let cancion = {
         cancion.titulo = nuevotitulo
     }
 }
-let contenedor = document.getElementById('track-list')
 
-axios.get('https://api.institutoalfa.org/api/songs').then((res) => {
-    console.log(res.data.songs)
-    res.data.songs.map(() => {
-        let div = document.createElement('div')
-        div.setAttribute('class', '')
-        div.innerHTML = `
-        <img src="/Ellipse 7.svg" alt="">
-        <p>Lost on You</p>
-        <p>LP</p>
-        <a href=""></a>
-    </div>
-
-    <div>
-        <img src="/Ellipse 8.svg" alt="">
-        <p>Papercut</p>
-        <p>Linkin Park</p>
-        <a href=""></a>
-    </div>
-
-    <div>
-        <img src="/Ellipse 10.svg" alt="">
-        <p>Dark Beach</p>
-        <p>Pastel ghost</p>
-        <a href=""></a>
-    </div>
-
-    <div>
-        <img src="/Ellipse 11.svg" alt="">
-        <p>Enamorado Tuyo</p>
-        <p>Cuarteto de Nos</p>
-        <a href=""></a>
-    </div>
-
-    <div>
-        <img src="/Ellipse 12.svg" alt="">
-        <p>Snowman</p>
-        <p>Sia</p>
-        <a href=""></a>
-    </div>
-
-    <div>
-        <img src="/Ellipse 9.svg" alt="">
-        <p>Stay with me</p>
-        <p>Miki Matsubara</p>
-        <a href=""></a>
-    </div>
-
-    <div>
-        <img src="/Ellipse 6.svg" alt="">
-        <p>Kingston Town</p>
-        <p>UB40</p>
-        contenedor.appendChild(div
-        `
-    })
-})
-
-let progress = document.getElementById("progress")
-        let song = document.getElementById("song")
-        let ctrlicon = document.getElementById("ctrlicon")
-
-        song.onloadedmetadata = function(){
-            progress.max = song.duration;
-            progress.value = song.currentTime;
-        }
-
-        function playPause(){
-            if(ctrlicon.classList.contains("fa-pause")){
-                song.pause();
-                ctrlicon.classList.remove("fa-pause");
-                ctrlicon.classList.add("fa-play");
-         }
-            else{
-                song.play();
-                ctrlicon.classList.add("fa-pause");
-                ctrlicon.classList.remove("fa-play");
-        }
-        
-        }
+       
